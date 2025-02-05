94d0fed9f0d5929aaf76c4a80d47f02ccc6964d8
Filipe Braganca <hello@filipe.co>
2025-02-05 11:01:12 -0500

    feat: add analytics and speedinsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import "./globals.css"
 import { Inter } from "next/font/google"
 import type React from "react"
 import { Toaster } from "@/components/ui/toaster"
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
@@ -20,6 +21,8 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
         <Toaster />
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )

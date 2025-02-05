b0c261cc5055d7889d3036dae0315da93688d919
Filipe Braganca <hello@filipe.co>
2025-02-04 20:09:36 -0500

    refactor: add logger to drizzle

--- drizzle/db.ts
+++ drizzle/db.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/vercel-postgres";
 import { sql } from "@vercel/postgres";
 import * as schema from "./schema";
 
-export const db = drizzle(sql, { schema });
+export const db = drizzle(sql, { schema, logger: true });
 
 export const getImages = async () => {
   return db.query.images.findMany();
